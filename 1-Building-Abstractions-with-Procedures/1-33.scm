88456a22882cbdf33a6da8769310aef2f6389db0
adaliu-gh <adaliu.gh@outlook.com>
2017-04-01 18:05:48 +0800

    add 1.33-b

--- 1-Building-Abstractions-with-Procedures/1-33.scm
+++ 1-Building-Abstractions-with-Procedures/1-33.scm
@@ -1,12 +1,15 @@
 (load "1-23.scm")
 
-(define (filtered-accumulate combiner null-value term a next b filter)
+(define (filtered-accumulate combiner null-value term a next b filter?)
   (define (iter a result)
     (cond
      [(> a b) result]
-     [(filter a) (iter (next a) (combiner result (term a)) )]
+     [(filter? a) (iter (next a) (combiner result (term a)) )]
      [else (iter (next a) result)]))
   (iter a null-value))
+
+;;========================
+;; a
 
 (define (sum-square-prime a b)
   (define square (lambda (x) (* x x)))
@@ -14,3 +17,20 @@
 
 (sum-square-prime 1 6)
 ;; => 39
+
+;;============================
+;; b
+
+(define (product-relative-prime n)
+  (define identity (lambda (x) x))
+  (define relative-prime? (lambda (x) (= 1 (gcd n x))))
+  (filtered-accumulate * 1 identity 1 add1 (sub1 n) relative-prime?))
+
+(define (gcd a b)
+  (if (zero? b) a
+      (gcd b (remainder a b))))
+
+(product-relative-prime 6)
+;; => 5
+
+
