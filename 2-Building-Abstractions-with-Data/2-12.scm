a07691fb93fb85e32841814cc3ff8d7b1984ec7b
adaliu-gh <adaliu.gh@outlook.com>
2017-04-05 21:01:13 +0800

    add 2.12

---
+++ 2-Building-Abstractions-with-Data/2-12.scm
@@ -0,0 +1,7 @@
+(load "2-11.scm")
+
+(define (make-center-percent center percent)
+  (let ((width (* center percent))))
+  (make-center-width center width))
+(define (percent i)
+  (/ (width i) (center i)))
