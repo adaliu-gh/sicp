84c898eb2217785e5e388334c399d520d44bfa35
adaliu-gh <adaliu.gh@outlook.com>
2017-04-29 16:31:33 +0800

    add 3.38-3.40

---
+++ chapter-3/3-3/3-38.scm
@@ -0,0 +1,27 @@
+;; result 1
+;; Peter -> Paul -> Mary
+;; 110 -> 90 -> 45
+
+;; result 2
+;; Peter -> Mary -> Paul
+;; 110 -> 55 -> 35
+
+;; result 3
+;; Paul -> Peter -> Mary
+;; 80 -> 90 -> 45
+
+;; result 4
+;; Paul -> Mary -> Peter
+;; 80 -> 40 -> 50
+
+;; result 5
+;; Mary -> Peter -> Paul
+;; 50 -> 60 -> 40
+
+;; result 6
+;; Mary -> Paul -> Peter
+;; 50 -> 30 -> 40
+
+
+;; concurrent
+;; either Mary does her transaction first or last
---
+++ chapter-3/3-3/concurrency-scripts.scm
@@ -0,0 +1,27 @@
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list #false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire)))
+            ((eq? m 'release)
+             (clear! cell))))
+    the-mutex))
+
+(define (clear! cell) (set-car! cell #false))
+
+(define (test-and-set! cell)
+  (without-interrupts
+   (if (car cell) #true
+       (begin (set-car! cell #true)
+              #false)) ))
