07f488a4add7a8db1998bd652d5b95bd6062bf44
adaliu-gh <adaliu.gh@outlook.com>
2017-05-21 08:53:33 +0800

    add 5.8 and codes in 5.2.2

--- chapter-5/5-1/machine-simulator.scm
+++ chapter-5/5-1/machine-simulator.scm
@@ -18,11 +18,11 @@
     (define (dispatch message)
       (cond ((eq? message 'get) contents)
             ((eq? message 'set)
-             (labmda (value) (set! contents value)))
+             (lambda (value) (set! contents value)))
             (else error "xxx" message)))
     dispatch))
 (define (get-contents register) (register 'get))
-(defien (set-contents! register value)
+(define (set-contents! register value)
   ((register 'set) value))
 
 ;; the stack
@@ -106,3 +106,158 @@
 
 ;;===========================
 ;; the assembler
+
+(define (assemble controller-text machine)
+  (extract-labels
+   controller-text
+   (lambda (insts labels)
+     (update-insts! insts labels machine)
+     insts)))
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels
+       (cdr text)
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+               (receive insts
+                   (cons (make-label-entry next-inst insts)
+                         labels))
+               (receive
+                   (cons (make-instruction next-inst)
+                         insts)
+                   labels)))))))
+
+(define (update-insts! insts labels machine)
+  (let ((pc (get-register machine 'pc))
+        (flag (get-register machine 'flag))
+        (stack (get-register machine 'stack))
+        (ops (machine 'operations)))
+    (for-each
+     (lambda (inst)
+       (set-instruction-execution-proc!
+        inst (make-execution-procedure
+              (instruction-text inst)
+              labels machine pc flag stack ops)))
+     insts)))
+
+(define (make-instruction text) (cons text '()))
+(define (instruction-text inst) (car inst))
+(define (instruction-execution-proc inst) (cdr inst))
+(define (set-instruction-execution-proc! inst proc)
+  (set-cdr! inst proc))
+
+(define (make-label-entry label-name insts)
+  (cons label-name insts))
+
+(define (lookup-label labels label-name)
+  (let ((val (assoc label-name labels)))
+    (if val
+        (cdr val)
+        (error "xxx"))))
+
+;;====================
+;; generating execution procedures for instructions
+
+(define (make-execution-procedure
+         inst labels machine pc flag stack ops)
+  (cond
+   ((eq? (car inst) 'assign)
+    (make-assign inst machine labels ops pc))
+   ((eq? (car inst) 'test)
+    (make-test inst machine labels ops flag pc))
+   ((eq? (car inst) 'goto)
+    (make-goto inst machine labels pc))
+   ((eq? (car inst) 'save)
+    (make-save inst machine stack pc))
+   ((eq? (car inst) 'restore)
+    (make-restore inst machine stack pc))
+   ((eq? (car inst) 'perform)
+    (make-perform inst machine labels ops pc))
+   (else (error "xxx"))))
+
+
+(define (make-assign inst machine labels operations pc)
+  (let ((target (get-register machine (assign-reg-name inst)))
+        (value-exp (assign-value-exp inst)))
+    (let ((value-proc
+           (if (operation-exp? value-exp)
+               (make-operation-exp
+                value-exp machine labels operations)
+               (make-primitive-exp
+                (car value-exp) machine labels))))
+      (lambda ()
+        (set-contents! target (value-proc))
+        (advance-pc pc)))))
+(define (assign-reg-name assign-instruction)
+  (cadr assign-instruction))
+(define (assign-value-exp assign-instruction)
+  (cddr assign-instruction))
+(define (advance-pc pc)
+  (set-contents! pc (cdr (get-contents pc))))
+
+
+(define (make-test inst machine labels operations flag pc)
+  (let ((condition (test-condition inst)))
+    (if (operation-exp? condition)
+        (let ((condition-proc
+               (make-operation-exp
+                condition machine labels operations)))
+          (lambda ()
+            (set-contents! flag (condition-proc))
+            (advance-pc)))
+        (error "xxx"))))
+(define (test-condition test-instruction)
+  (cdr test-instruction))
+
+(define (make-branch inst machine labels flag pc)
+  (let ((dest (branch-dest inst)))
+    (if (label-exp? dest)
+        (let ((insts (lookup-label labels (label-exp-label dest))))
+          (lambda ()
+            (if (get-contents flag)
+                (set-contents! pc insts)
+                (advance-pc pc))))
+        (error "xxx"))))
+(define (branch-dest branch-instruction)
+  (cadr branch-instruction))
+
+(define (make-goto inst machine labels pc)
+  (let ((dest (goto-test inst)))
+    (cond ((label-exp? dest)
+           (let ((insts (lookup-label labels (label-exp-label dest))))
+             (lambda () (set-contents! pc insts))))
+          ((register-exp? dest)
+           (let ((reg (get-register
+                       machine (register-exp-reg dest))))
+             (lambda ()
+               (set-contents! pc (get-contents reg)))))
+          (else (error "xxx")))))
+(define (goto-test goto-instruction)
+  (cadr goto-instruction))
+
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (push stack (get-contents reg))
+      (advance-pc pc))))
+(define (make-restore inst machine stack pc)
+  (let ((reg (get-register machine (stack-inst-reg-name inst))))
+    (lambda ()
+      (set-contents! reg (pop stack))
+      (advance-pc pc))))
+(define (stack-inst-reg-name stack-instruction)
+  (cadr stack-instruction))
+
+(define (make-perform inst machine labels operations pc)
+  (let ((action (perform-action inst)))
+    (if (operation-exp? action)
+        (let ((action-proc (make-operation-exp action machine labels operations)))
+          (lambda ()
+            (action-proc) (advance-pc pc)))
+        (error "xxx"))))
+(define (perform-action inst) (cdr inst))
+
